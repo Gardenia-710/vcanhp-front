0e7b252e436b7bf5444d72f409f9f8080ba81ca7
Gardenia-710 <56499607+Gardenia-710@users.noreply.github.com>
2024-06-22 09:15:48 +0900

    SSRにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["assets.newt.so"],
+    domains: ["assets.newt.so"]
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()],
+  output: "hybrid",
+  adapter: cloudflare()
 });
