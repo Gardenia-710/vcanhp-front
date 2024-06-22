57bfc5a1974f888b2d60eaf24c36c60a754742a2
Gardenia-710 <56499607+Gardenia-710@users.noreply.github.com>
2024-06-22 09:42:10 +0900

    vercelにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +13,5 @@ export default defineConfig({
     applyBaseStyles: false
   }), react()],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: vercel()
 });
